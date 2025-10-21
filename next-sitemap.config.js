104d8d330443fe06325976fab823788af8f60933
dgsa8116-crypto <dgsa8116@gmail.com>
2025-10-21 19:08:00 +0800

    Add files via upload

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://tntlinebotseemyeyes.online',
-  generateRobotsTxt: true
-}
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  exclude: ['/404'],
+  // ✅ 新增這行讓 next-sitemap 自動找到新的輸出資料夾
+  outDir: 'out',
+};
