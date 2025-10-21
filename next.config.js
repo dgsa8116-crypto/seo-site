04c61be9a0e70e11a3d029ae56c7178376c9f9a9
dgsa8116-crypto <dgsa8116@gmail.com>
2025-10-21 19:27:36 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
+  output: 'export', // ← 靜態輸出
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
